654be54804f9eb3ac5c5486239d2ab39ebec006c
Slava Torchik <torhik_slava@mail.ru>
2020-03-02 21:34:54 +0300

    feat: implement brackets-checker

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let length = str.length;
+  if(length % 2 != 0) return false;
+  let s = str;
+  let config = bracketsConfig.map(item => item.join(''));  // transform to type: ['{}', '[]', '()']
+  while(true) {
+    for (let i = 0; i < config.length; i++) {
+      if(s.indexOf(config[i]) >= 0) s=s.replace(config[i], '');
+    }
+    if (s.length == length) break;
+    length = s.length;
+  }  
+  return s.length == 0 ? true : false; 
 }
